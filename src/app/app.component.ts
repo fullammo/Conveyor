2c5fb4db2d6269a0a0e28341f1e2202e736b4f2b
fullammo <bendegulyas1@gmail.com>
2019-01-29 18:21:34 +0100

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'conv-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'Conveyor';
+}
