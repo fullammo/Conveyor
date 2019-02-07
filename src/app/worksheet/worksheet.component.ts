e30cdde10253a4b6e5f5b4403aadf24caa180ded
fullammo <bendegulyas1@gmail.com>
2019-02-07 22:15:22 +0100

    Column adding and removing works by buttons on datatable

--- src/app/worksheet/worksheet.component.ts
+++ src/app/worksheet/worksheet.component.ts
@@ -3,26 +3,166 @@ import { WorkSheetItem } from '../models/worksheet';
 import { MomentService } from '../providers/moment.service';
 
 const EXAMPLE_DATA: WorkSheetItem[] = [
-  { actualDay: 1, cashierNumber: 1 },
-  { actualDay: 2, cashierNumber: 1 },
-  { actualDay: 3, cashierNumber: 1 },
-  { actualDay: 4, cashierNumber: 1 },
-  { actualDay: 5, cashierNumber: 1 },
-  { actualDay: 6, cashierNumber: 1 },
-  { actualDay: 7, cashierNumber: 1 },
-  { actualDay: 8, cashierNumber: 1 },
-  { actualDay: 9, cashierNumber: 1 },
-  { actualDay: 10, cashierNumber: 1 },
-  { actualDay: 11, cashierNumber: 1 },
-  { actualDay: 12, cashierNumber: 1 },
-  { actualDay: 13, cashierNumber: 1 },
-  { actualDay: 14, cashierNumber: 1 },
-  { actualDay: 15, cashierNumber: 1 },
-  { actualDay: 16, cashierNumber: 1 },
-  { actualDay: 17, cashierNumber: 1 },
-  { actualDay: 18, cashierNumber: 1 },
-  { actualDay: 19, cashierNumber: 1 },
-  { actualDay: 20, cashierNumber: 1 }
+  {
+    actualDay: 1,
+    cashierNumber: 1,
+    income0: 0,
+    income5: 2,
+    income18: 3,
+    income27: 20
+  },
+  {
+    actualDay: 2,
+    cashierNumber: 1,
+    income0: 0,
+    income5: 2,
+    income18: 3,
+    income27: 20
+  },
+  {
+    actualDay: 3,
+    cashierNumber: 1,
+    income0: 0,
+    income5: 2,
+    income18: 3,
+    income27: 20
+  },
+  {
+    actualDay: 4,
+    cashierNumber: 1,
+    income0: 0,
+    income5: 2,
+    income18: 3,
+    income27: 20
+  },
+  {
+    actualDay: 5,
+    cashierNumber: 1,
+    income0: 0,
+    income5: 2,
+    income18: 3,
+    income27: 20
+  },
+  {
+    actualDay: 6,
+    cashierNumber: 1,
+    income0: 0,
+    income5: 2,
+    income18: 3,
+    income27: 20
+  },
+  {
+    actualDay: 7,
+    cashierNumber: 1,
+    income0: 0,
+    income5: 2,
+    income18: 3,
+    income27: 20
+  },
+  {
+    actualDay: 8,
+    cashierNumber: 1,
+    income0: 0,
+    income5: 2,
+    income18: 3,
+    income27: 20
+  },
+  {
+    actualDay: 9,
+    cashierNumber: 1,
+    income0: 0,
+    income5: 2,
+    income18: 3,
+    income27: 20
+  },
+  {
+    actualDay: 10,
+    cashierNumber: 1,
+    income0: 0,
+    income5: 2,
+    income18: 3,
+    income27: 20
+  },
+  {
+    actualDay: 11,
+    cashierNumber: 1,
+    income0: 0,
+    income5: 2,
+    income18: 3,
+    income27: 20
+  },
+  {
+    actualDay: 12,
+    cashierNumber: 1,
+    income0: 0,
+    income5: 2,
+    income18: 3,
+    income27: 20
+  },
+  {
+    actualDay: 13,
+    cashierNumber: 1,
+    income0: 0,
+    income5: 2,
+    income18: 3,
+    income27: 20
+  },
+  {
+    actualDay: 14,
+    cashierNumber: 1,
+    income0: 0,
+    income5: 2,
+    income18: 3,
+    income27: 20
+  },
+  {
+    actualDay: 15,
+    cashierNumber: 1,
+    income0: 0,
+    income5: 2,
+    income18: 3,
+    income27: 20
+  },
+  {
+    actualDay: 16,
+    cashierNumber: 1,
+    income0: 0,
+    income5: 2,
+    income18: 3,
+    income27: 20
+  },
+  {
+    actualDay: 17,
+    cashierNumber: 1,
+    income0: 0,
+    income5: 2,
+    income18: 3,
+    income27: 20
+  },
+  {
+    actualDay: 18,
+    cashierNumber: 1,
+    income0: 0,
+    income5: 2,
+    income18: 3,
+    income27: 20
+  },
+  {
+    actualDay: 19,
+    cashierNumber: 1,
+    income0: 0,
+    income5: 2,
+    income18: 3,
+    income27: 20
+  },
+  {
+    actualDay: 20,
+    cashierNumber: 1,
+    income0: 0,
+    income5: 2,
+    income18: 3,
+    income27: 20
+  }
 ];
 
 @Component({
@@ -33,10 +173,10 @@ const EXAMPLE_DATA: WorkSheetItem[] = [
 export class WorksheetComponent implements OnInit {
   workSheetItems: WorkSheetItem[] = EXAMPLE_DATA;
 
-  readonly zeroIncomeColumnName: string = 'Bevétel 0%';
-  readonly fiveIncomeColumnName: string = 'Bevétel 5%';
-  readonly eighteenIncomeColumnName: string = 'Bevétel 18%';
-  readonly twentySevenIncomeColumnName: string = 'Bevétel 27%';
+  readonly zeroIncomeColumnName: string = 'income0';
+  readonly fiveIncomeColumnName: string = 'income5';
+  readonly eighteenIncomeColumnName: string = 'income18';
+  readonly twentySevenIncomeColumnName: string = 'income27';
 
   public zeroIncomeColumDisplayed = true;
   public fiveIncomeColumnDisplayed = true;
@@ -47,25 +187,25 @@ export class WorksheetComponent implements OnInit {
 
   /** Columns displayed in the table. Columns IDs can be added, removed, or reordered. */
   displayedColumns: string[] = [
-    'Nap',
-    'Pt.gép' //,
-    // this.zeroIncomeColumnName,
-    // this.fiveIncomeColumnName,
-    // this.eighteenIncomeColumnName,
-    // this.twentySevenIncomeColumnName
+    'actualDay',
+    'cashierNumber',
+    this.zeroIncomeColumnName,
+    this.fiveIncomeColumnName,
+    this.eighteenIncomeColumnName,
+    this.twentySevenIncomeColumnName
   ];
 
   columnsToDisplay: string[] = this.displayedColumns.slice();
 
   addIncomeColumn(columnName: string): void {
-    // this.columnsToDisplay.push(columnName);
+    this.columnsToDisplay.push(columnName);
     this.toggleDisplayStatus(columnName);
   }
 
   removeIncomeColumn(columnName: string): void {
-    // this.columnsToDisplay = this.columnsToDisplay.filter(
-    //   column => column !== columnName
-    // );
+    this.columnsToDisplay = this.columnsToDisplay.filter(
+      column => column !== columnName
+    );
     this.toggleDisplayStatus(columnName);
   }
 
@@ -77,6 +217,13 @@ export class WorksheetComponent implements OnInit {
       case this.fiveIncomeColumnName:
         this.fiveIncomeColumnDisplayed = !this.fiveIncomeColumnDisplayed;
         break;
+      case this.eighteenIncomeColumnName:
+        this.eighteenIncomeColumnDisplayed = !this
+          .eighteenIncomeColumnDisplayed;
+        break;
+      case this.twentySevenIncomeColumnName:
+        this.twentySevenIncomeColumnDisplayed = !this
+          .twentySevenIncomeColumnDisplayed;
     }
   }
 
